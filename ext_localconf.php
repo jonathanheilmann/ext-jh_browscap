c26163fe0ce4a4f0e41d2e68abc3abd820a53a8c
jonathanheilmann <mail@jonathan-heilmann.de>
2016-06-10 18:23:58 +0200

    [TASK] Code clean up

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['Heilmann\JhBrowscap\Task\UpdateBrowscapIni'] = array(
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\Heilmann\JhBrowscap\Task\UpdateBrowscapIni::class] = array(
     'extension' => $_EXTKEY,
     'title' => 'Update Browscap.ini',
     'description' => 'This task updates your local Browscap.ini',
